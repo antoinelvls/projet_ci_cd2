5ff494190dd676df4e944de088ff56482b89d044
Antoine Levallois <wkainformatique@gmail.com>
2020-10-12 12:42:50 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,14 @@ variable "aws_region" {
   default = "eu-west-3"
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  default = "AKIA2G2XJY5PRHXUEGWX"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = "53X7dR90ZfLToBljtYFjaVe/PNGFgSQeHz6o3LBC"
+}
+
 variable "runtime" {
   type    = string
   default = "python3.8"
